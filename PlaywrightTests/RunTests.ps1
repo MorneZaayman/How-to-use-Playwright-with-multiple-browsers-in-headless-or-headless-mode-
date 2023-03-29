7a4e703c531355dcb1cd123afb8b0374acf29170
Morné Zaayman <mornezaayman@morzgroup.com>
2023-03-29 20:48:31 +0200

    Add project files.

---
+++ PlaywrightTests/RunTests.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host 'Running Playwright Tests'
+dotnet test --settings:runsettings/chromium.runsettings
+dotnet test --settings:runsettings/firefox.runsettings
+dotnet test --settings:runsettings/webkit.runsettings
+Read-Host "Press any key to quit."
